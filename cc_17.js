002b3e325f4e831ba756f0784394c0fba725aa53
Natalie Devenbeck <devenbeck@usf.edu>
2025-03-30 18:35:35 -0400

    Task 4 – Finalized reporting system

--- cc_17.js
+++ cc_17.js
@@ -56,3 +56,31 @@ const vipCustomer1 = new VIPCustomer("Dave Bogner", "dave123bogner@gmail.com","G
 vipCustomer1.addPurchase(200);   //lot of zeros after 220 in console
 console.log(`New VIP Customer created: ${vipCustomer1.name} (${vipCustomer1.vipLevel})`); 
 console.log(`Total Spent by ${vipCustomer1.name} (VIP ${vipCustomer1.vipLevel}) was $${vipCustomer1.getTotalSpent()}`); 
+
+//Task 4: Build a Client Report System
+//Store multiple customers (regular & vip and a sales rep)
+const customer2 = new Customer("Andrew Schulyer", "asdschulyer@gmail.com");
+const vipCustomer2 = new VIPCustomer("Amanda Tepole", "amandatp@gmail.com", "Platinum");
+customer2.addPurchase(75);
+vipCustomer2.addPurchase(500);
+
+const customers = [customer1, customer2, vipCustomer1, vipCustomer2];
+
+// Create a SalesRep and assign customers to him
+const salesRep2 = new SalesRep("Liam MacRoy");
+salesRep2.addClient(customer2);
+salesRep2.addClient(vipCustomer2);
+const salesReps = [salesRep1, salesRep2];
+const totalRevenue = customers.reduce((total, customer) => total + customer.getTotalSpent(), 0);
+const highSpenders = customers.filter(customer => customer.getTotalSpent() > 500); //find the high spending customers
+// Create customer summary
+const customerSummary = customers.map(customer => ({
+  name: customer.name,
+  totalSpent: customer.getTotalSpent()
+}));
+// Log:  Total revenue, High-spending customers, Customer summary
+console.log(`Total Revenue: $${totalRevenue}`);
+console.log("High-Spending Customers:");
+highSpenders.forEach(customer => console.log(`${customer.name} - $${customer.getTotalSpent()}`));
+console.log("Customer Summary:");
+customerSummary.forEach(summary => console.log(`Name: ${summary.name}, Total Spent: $${summary.totalSpent}`));
